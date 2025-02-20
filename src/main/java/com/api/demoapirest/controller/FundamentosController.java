4571efe03a713ecaccee89ebda605de86c6bde3b
ahiltonplay <ahiltonplayy@gmail.com>
2025-02-20 09:41:27 -0500

    controlador

--- src/main/java/com/api/demoapirest/controller/FundamentosController.java
+++ src/main/java/com/api/demoapirest/controller/FundamentosController.java
@@ -1,4 +1,15 @@
 package com.api.demoapirest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/fundamentos")
 public class FundamentosController {
+
+    @GetMapping("/ejercicio1")
+    public String ejercicio1(){
+        return "Hola Mundo REST";
+    }
 }
