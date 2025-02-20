20bd10a72edcacca5abc2a141b94b9fda6f245c0
ahiltonplay <ahiltonplayy@gmail.com>
2025-02-20 10:03:32 -0500

    algoritmos del servicio finalizados

--- src/main/java/com/api/demoapirest/service/IFundamentosService.java
+++ src/main/java/com/api/demoapirest/service/IFundamentosService.java
@@ -1,4 +1,11 @@
 package com.api.demoapirest.service;
 
+import java.util.List;
+
 public interface IFundamentosService {
+
+    String validarNumeroPrimo(int numero);
+    String calcularFactorial(int numero);
+    List<Integer> calcularFibonacci(int numero);
+
 }
